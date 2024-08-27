598b44dc0c854df17873a36f39cf763fcf1c1309
ayesha19297 <ayesha19297@gmail.com>
2024-08-27 11:46:26 +0530

    initialized react app

---
+++ git-extension/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="App-header">Welcome to git extension of vs code</h2>
+    </div>
+  );
+}
+
+export default App;
