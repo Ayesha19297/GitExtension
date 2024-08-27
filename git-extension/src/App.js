043bc3d3619582bef10fcd05bb8d60df8af57a77
ayesha19297 <ayesha19297@gmail.com>
2024-08-27 13:18:15 +0530

    tailwind initialization

--- git-extension/src/App.js
+++ git-extension/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import ChatInterface from "./Components/ChatInterface";
 
 function App() {
   return (
     <div className="App">
       <h2 className="App-header">Welcome to git extension of vs code</h2>
+      <ChatInterface />
     </div>
   );
 }
